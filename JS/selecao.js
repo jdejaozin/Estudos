f8260f2ccd241d1b2037b156d6e9c379e8adae9a
João Pedro <80296137+jdejaozin@users.noreply.github.com>
2021-03-29 10:24:24 -0300

    Atualização 0.2

--- JS/selecao.js
+++ JS/selecao.js
@@ -1,27 +1,79 @@
-let imagem1 = document.querySelector(".imagem")
-let humano = document.querySelector(".humano")
-let anao = document.querySelector(".anao")
-let elfo = document.querySelector(".elfo")
+const option = document.querySelector('.opt');
+const imageShow = document.querySelector(".image");
+const personClass = document.querySelector('.personClass')
+const human = document.querySelector("#human");
+const dwarf = document.querySelector("#dwarf");
+const elf = document.querySelector("#elf");
+const personName = document.querySelector("#personName")
 
-let valorHumano = humano.getAttribute('aria-checked');
-let valorAnao = anao.getAttribute('aria-checked');
-let valorElfo = elfo.getAttribute('aria-checked');
-let imagem='';
+let imageHTML='';
 
-function verificarHumano(){
-    if(valorHumano !== true){
-        imagem = "<div class='imagem'><img src='images/human.png' alt=''></div>"
+function mouseIn(event){
+    if(human.checked === true || dwarf.checked === true || elf.checked === true){
+
+    }else{
+        if(event.target.id === 'human'){
+            imageHTML = "<div class='image'><img src='images/human.png' alt=''></div>"
+            imageShow.innerHTML = imageHTML
+        }
+        else if(event.target.id === 'elf'){
+            imageHTML = "<div class='image'><img src='images/elfo.png' alt=''></div>"
+            imageShow.innerHTML = imageHTML
+        }
+        else if(event.target.id === 'dwarf'){
+            imageHTML = "<div class='image'><img src='images/anao.png' alt=''></div>"
+            imageShow.innerHTML = imageHTML
+        }
     }
-    imagem1.innerHTML= imagem
 }
-function verificarAnao(){
-    imagem = "<div class='imagem'><img src='images/anao.png' alt=''></div>"
-    imagem1.innerHTML= imagem
-}
-function verificarElfo(){
-    if(valorElfo !== true){
-        imagem = "<div class='imagem'><img src='images/elfo.png' alt=''></div>"
+
+function mouseOut(){
+    if(human.checked === true || dwarf.checked === true || elf.checked === true){
+        
+    }else{
+        imageShow.innerHTML = ''
     }
-    imagem1.innerHTML= imagem
 }
-console.log(valorHumano)
+
+function checky(){
+    if(human.checked){
+        imageHTML = "<div class='imagem'><img src='images/human.png' alt=''></div>"
+        imageShow.innerHTML= imageHTML
+        personClass.innerHTML = `
+        Classe</br>
+        <input id='warrior' type="radio" name='personClass'>
+        <label for="#warrior">Guerreiro</label><br/>
+        <input id='archer' type='radio' name='personClass'>
+        <label for="#archer">Arqueiro</label><br/>
+        <input id='wizard' type='radio' name='personClass'>
+        <label for="#wizard">Mago</label><br/>`
+    }
+    else if(dwarf.checked){
+        imageHTML = "<div class='imagem'><img src='images/anao.png' alt=''></div>"
+        imageShow.innerHTML= imageHTML
+        personClass.innerHTML = `
+        Classe</br>
+        <input id='warrior' type="radio" name='personClass'>
+        <label for="#warrior">Só pra mostrar</label><br/>
+        <input id='archer' type='radio' name='personClass'>
+        <label for="#archer">Que da pra</label><br/>
+        <input id='wizard' type='radio' name='personClass'>
+        <label for="#wizard">Fazer um pra cada</label><br/>`
+    }
+    else if(elf.checked){
+        imageHTML = "<div class='imagem'><img src='images/elfo.png' alt=''></div>"
+        imageShow.innerHTML= imageHTML
+        personClass.innerHTML = `
+        Classe</br>
+        <input id='warrior' type="radio" name='personClass'>
+        <label for="#warrior">lorem</label><br/>
+        <input id='archer' type='radio' name='personClass'>
+        <label for="#archer">ipsum</label><br/>
+        <input id='wizard' type='radio' name='personClass'>
+        <label for="#wizard">coisas em latim são chiques</label><br/>`
+    }
+}
+
+option.addEventListener('click', checky, false)
+option.addEventListener('mouseover', mouseIn, false)
+option.addEventListener('mouseout', mouseOut, false)
